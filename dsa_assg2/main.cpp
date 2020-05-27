1c271eeee563e21d527cdb657b32406e4b88cccf
Phuong Doan <35623720+phuongdoan13@users.noreply.github.com>
2020-05-27 17:26:28 +1000

    Initial Commit

---
+++ dsa_assg2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dsa_assg2
+//
+//  Created by ヴァイオレット・エヴァーガーデン on 27/5/20.
+//  Copyright © 2020 Harry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
